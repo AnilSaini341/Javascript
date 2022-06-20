2733538fa3132a4a9386a667152e4fa42272433b
AnilSaini <Anil.Saini@mashvirtual.in>
2022-06-20 16:34:51 +0530

    target DOM Elements

---
+++ js/dom_js.js
@@ -0,0 +1,16 @@
+var element;
+//element=document.all; //return all tags
+//element=document.head; // return head tag
+//element=document.title; // return title tag
+//element=document.body; // return body tag
+//element=document.links; // return links tag
+//element=document.images; // return links tag
+//element=document.doctype; // return doctype tag
+//element=document.URL; // return URL tag
+//element=document.domain; // return domain tag
+//element=document.baseURI; // return baseURI tag
+//element=document.getElementById("top_section"); // return top_section id tag
+//element=document.getElementsByClassName("w3-jumbo"); // return Class Name tag
+element=document.getElementsByTagName("h1"); // return Tag Name tag
+
+console.log(element);
