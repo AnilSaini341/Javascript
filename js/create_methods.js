39145969591bb6293690afc888bba7f0fc0d37e6
AnilSaini <Anil.Saini@mashvirtual.in>
2022-07-05 16:31:15 +0530

    createElement, createTextNode, createComment

---
+++ js/create_methods.js
@@ -0,0 +1,8 @@
+var element;
+element=document.createElement("p"); //createElement create a html tag
+var createText=document.createTextNode("this is a text node"); //createTextNode create a html text node
+var createComment=document.createComment("this is a comment"); //createComment create comment
+
+console.log(element);
+console.log(createText);
+console.log(createComment);
