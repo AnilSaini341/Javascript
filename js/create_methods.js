050bcda09d873e1e822e9cceae0648bf9a6ef422
AnilSaini <Anil.Saini@mashvirtual.in>
2022-07-05 16:52:38 +0530

    appendChild, insertBefore

--- js/create_methods.js
+++ js/create_methods.js
@@ -1,7 +1,14 @@
 var element;
-element=document.createElement("p"); //createElement create a html tag
-var createText=document.createTextNode("this is a text node"); //createTextNode create a html text node
-var createComment=document.createComment("this is a comment"); //createComment create comment
+element=document.createElement("h2"); //createElement create a html tag
+var createText=document.createTextNode("This is a text node"); //createTextNode create a html text node
+var createComment=document.createComment("This is a comment"); //createComment create comment
+
+element.appendChild(createText);  //appendChild used to add element tag and element text node.
+
+document.getElementById("text").appendChild(element); //appendChild also used to add complete HTML tag  ( appendChild adds in last of tag )  
+
+var target =document.getElementById("text");
+target.insertBefore(element,target.childNodes[0]); // insertBefore adds html node with given position
 
 console.log(element);
 console.log(createText);
