176f5dc0be6a04e9e1cb2a4024d6af7b9b901479
AnilSaini <Anil.Saini@mashvirtual.in>
2022-07-25 10:34:01 +0530

    has Attribute

---
+++ js/hasAttribute.js
@@ -0,0 +1,6 @@
+var element;
+
+var parentElement = document.getElementById("test");
+var find=parentElement.hasAttribute("class"); //hasAttribute Method check weather attribute is present or not and returns true or false 
+
+console.log(find);
