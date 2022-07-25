69b9f0462ef931a36ae759571e047e25aacd5c2f
AnilSaini <Anil.Saini@mashvirtual.in>
2022-07-25 10:39:16 +0530

    has Child node

--- js/hasAttribute.js
+++ js/hasAttribute.js
@@ -2,5 +2,8 @@ var element;
 
 var parentElement = document.getElementById("test");
 var find=parentElement.hasAttribute("class"); //hasAttribute Method check weather attribute is present or not and returns true or false 
-
 console.log(find);
+
+var haschildnode=parentElement.hasChildNodes(); //hasAttribute Method check weather any child node is present or not and returns true or false 
+console.log(haschildnode);
+
